0add39553c687f5a9bf89c94bb7c0726eb8f9d2f
Raftech1723 <rjmp.rjmp.ingenieriaproyectos@gmail.com>
2023-11-16 18:14:36 -0500

    se esta eleborando los comandos para interactura con la BD

--- src/form.js
+++ src/form.js
@@ -1,10 +1,22 @@
-console.log("Hola Mundo");
+//Importación de paquetes
+const express = require("express")
+const mongoose = require("mongoose")
 
-var a=2;
-var b=3;
 
-function suma(){
-    console.log(a+b);
-}
+//Definición de configuraciones
+const app = express()
+const puerto = 4000;
 
-suma();
+//rutas
+app.get("/prueba", (req, res) => {res.send("Página de prueba secundaria")})
+app.get("/", (req, res) => {res.send("Página principal")})
+
+
+
+//Ejecución
+
+mongoose.connect("mongodb+srv://Smartcode:smarcode@dbcluster.zbppxht.mongodb.net/?retryWrites=true&w=majority")
+    .then(() => {console.log("Conexión realizado con éxito")})
+    .catch((error)=> {console.log("error")})
+app.listen(puerto, () => {console.log("servidor escuchando en el puerto" +puerto)})
+
