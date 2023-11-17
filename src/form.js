831de86ffe3280254ccd9244f9ca17bf54a42b6e
Raftech1723 <rjmp.rjmp.ingenieriaproyectos@gmail.com>
2023-11-17 10:38:47 -0500

    se cargo nuevos paquetes

--- src/form.js
+++ src/form.js
@@ -2,20 +2,19 @@
 const express = require("express")
 const mongoose = require("mongoose")
 
-
 //Definición de configuraciones
-const app = express()
+const app = express();
 const puerto = 4000;
 
 //rutas
-app.get("/prueba", (req, res) => {res.send("Página de prueba secundaria")})
-app.get("/", (req, res) => {res.send("Página principal")})
+app.get("/prueba", (req, res) => {res.send("Página de prueba secundaria")});
+app.get("/", (req, res) => {res.send("Página principal")});
 
 
 
 //Ejecución
 
-mongoose.connect("mongodb+srv://Smartcode:smarcode@dbcluster.zbppxht.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://Smartcode:Smartcode@dbcluster.zbppxht.mongodb.net/?retryWrites=true&w=majority")
     .then(() => {console.log("Conexión realizado con éxito")})
     .catch((error)=> {console.log("error")})
 app.listen(puerto, () => {console.log("servidor escuchando en el puerto" +puerto)})
